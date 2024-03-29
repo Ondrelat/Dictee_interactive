fd5b11d541f286681fd0291d2425302f4f897fdd
Ondrelat <avrillon.benoit@orange.fr>
2024-03-29 09:11:18 +0100

    audiochange

--- app/ui/dictation/audio.tsx
+++ app/ui/dictation/audio.tsx
@@ -1,6 +1,6 @@
 
 import { dictation } from '@prisma/client'
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useLayoutEffect} from 'react';
 
 interface AudioProps {
     dictation: dictation;
@@ -10,26 +10,29 @@ interface AudioProps {
 export default function Audio({ dictation, audioIndex }: AudioProps) {
 
     const audioRef = useRef<HTMLAudioElement>(null);
-    const isFirstRender = useRef(true);
 
     // Construction de l'URL du fichier audio avec encodage des caractères TEST
     const audioSrc = `${dictation.audio_url}/${dictation.title}_partie_${audioIndex}.mp3`;
     console.log("test : " + audioSrc)
+    const firstUpdate = useRef(true);
 
-    useEffect(() => {
-        if (isFirstRender.current) {
-            isFirstRender.current = false;
-          } else {
+    useLayoutEffect(() => {
+        // Vérifiez en plus si playAudio est true avant de jouer l'audio
+        console.log("firstUpdate", firstUpdate)
+        if (!firstUpdate.current) {
+            console.log("audioRef", audioRef)
             if (audioRef.current) {
-              audioRef.current.play();
+                audioRef.current.play();
             }
-          }
-      }, [audioIndex]);
+        } else {
+            firstUpdate.current = false;
+        }
+      }, [audioIndex, firstUpdate]); 
 
     if (dictation && dictation.audio_url) {
         return (
             <>
-                <audio src="audios/Un_Jour_à_la_Ferme_partie_1.mp3" controls ref={audioRef} className="border-2 border-black rounded-full text-base" />
+                <audio src={audioSrc} controls ref={audioRef} className="border-2 border-black rounded-full text-base" />
             </>
         );
     } else {
