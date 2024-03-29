da23ebc64c67519795ea0335646f1a2ad33e042a
Ondrelat <avrillon.benoit@orange.fr>
2024-03-29 19:26:26 +0100

    responsive

--- app/ui/dictation/dictation.tsx
+++ app/ui/dictation/dictation.tsx
@@ -26,22 +26,23 @@ export default function Dictations({ initialDictationData }: Props) {
 
     return (
         <>
-          <main>
-            <div className="flex h-2/5 flex-row">
-              <div className="flex flex-grow justify-center items-center bg-lightblue-200 flex-1 z-1">
-                  <div style={{ position: 'relative', top:"10%", left:"30%" }} >
-                    <Image src="/images/micro.jpg" width={500} height={460} alt="Image de microphone pour l'audio de la dictée" />
-                    <div style={{ position: 'absolute', left: '50%', top: '40%', transform:'translate(-50%, -50%)' }}>
-                      <Audio dictation={initialDictationData} audioIndex={audioIndex} />
-                    </div>
-                  </div>
-              </div>
-              <div className="flex flex-grow justify-center items-center bg-lightred-200 flex-2 z-2">
-                  <UserInput dictationText={initialDictationData.text} validateSentencePart={handleNextAudio} />
-              </div>
-              <div className="flex flex-grow justify-center items-center bg-lightgreen-200 flex-1"></div>
-            </div>
-          </main>
+<main>
+  <div className="flex flex-col md:flex-row h-2/5">
+    <div className="flex flex-grow justify-center items-center bg-lightblue-200 md:flex-1 z-10 relative">
+      <div className="relative responsive-position" style={{ position: 'relative', top:"10%", left:"30%" }} >
+         <Image src="/images/micro.jpg" width={500} height={460} alt="Image de microphone pour l'audio de la dictée" />
+          <div style={{ position: 'absolute', left: '50%', top: '40%', transform:'translate(-50%, -50%)' }}>
+            <Audio dictation={initialDictationData} audioIndex={audioIndex} />
+          </div>
+      </div>
+    </div>
+    <div className="flex flex-grow justify-center items-center bg-lightred-200 md:flex-2 z-20">
+      <UserInput dictationText={initialDictationData.text} validateSentencePart={handleNextAudio} />
+    </div>
+    <div className="flex flex-grow justify-center items-center bg-lightgreen-200 md:flex-1"></div>
+  </div>
+</main>
+
         </>
     );
 }
