be9509b55b54f6eaafbe73e7560a9f000759088d
Ondrelat <avrillon.benoit@orange.fr>
2024-03-30 16:20:33 +0100

    redirect le home

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
+import { redirect } from 'next/navigation'
 export default async function Home() {
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
-    </main>
+    redirect('/dictee')
   );
 }
