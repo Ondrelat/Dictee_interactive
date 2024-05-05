57bf998ec520706602c1a354dc688ecdefef310c
Ondrelat <avrillon.benoit@orange.fr>
2024-05-05 18:01:02 +0200

    Correction balise meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,25 +3,25 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import SessionWrapper from './components/sessionWraper';
 import Navbar from './ui/navbar';
-import Head from 'next/head';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Dictée Interactive",
+  description: "Avec Dictée Interactive, améliorez votre orthographe grâce à des dictées avec correction en temps réel et des aides interactives personnalisées.",
+  keywords: "Dictée, Orthographe, dictée interactive, dictée audio en ligne, Correction en temps réel, Dictée en temps réel",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{ children: React.ReactNode; }>) {
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="fr">
-      <Head>
-        <title>Dictée Interactive</title>
-        <meta name="keywords" content="Dictée, Orthographe, dictée interactive, dictée audio en ligne, Correction en temps réel, Dictée en temps réel" />
-        <meta name="description" content="Avec Dictée Interactive, améliorez votre orthographe grâce à des dictées avec correction en temps réel et des aides interactives personnalisées." />
-        <link rel="icon" href="/favicon.ico" type="x-icon" />
-      </Head>
       <SessionWrapper>
         <body className={inter.className}>
           <Navbar />
