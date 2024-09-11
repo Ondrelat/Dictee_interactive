c81ce0c50cfd1c6503a1a9bd52d9e6bd2933b378
Ondrelat <avrillon.benoit@orange.fr>
2024-09-11 21:02:40 +0900

    discord

--- app/footer.tsx
+++ app/footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
           <p>&copy; {new Date().getFullYear()} Dictée intéractive.</p>
         </div>
         <div>
-          <Link href="https://discord.gg/dftkVKGx" target="_blank" rel="noopener noreferrer">
+          <Link href="https://discord.gg/6uWrYH6s" target="_blank" rel="noopener noreferrer">
             <div className="relative w-[30px] h-[30px]">
               <Image
                 src="/discord-logo_3.png"
