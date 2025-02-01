ae47cab5de6af9b46721dfa67d8cb97a08f265c9
Ondrelat <avrillon.benoit@orange.fr>
2025-02-01 09:51:08 +0100

    changement de mail

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -9,10 +9,10 @@ export default async function Home() {
     <div className="page-container">
       <div className="email-container">
         <p className="text">Pour me contacter, envoyez-moi un mail :</p>
-        <p className="email">Dictee.Interactive@gmail.com</p>
+        <p className="email">avrillon.benoit73@gmail.com</p>
       </div>
       <div className="mt-4 flex justify-center">
-        Mais si tu veux avoir plus de chances que je réponde, rejoins-moi sur Discord.
+        Tu peux aussi me rejoindre sur Discord.
         <Link href="https://discord.gg/HWfnzXPeZU" target="_blank" rel="noopener noreferrer">
           <div className="relative w-[30px] h-[30px]">
             <Image
