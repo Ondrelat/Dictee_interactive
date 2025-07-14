e8ed4b5fbbe5d50a8008fd8ed16b55f880b29a30
Ondrelat <avrillon.benoit73@gmail.com>
2025-07-14 13:06:37 +0200

    modification page d'accueil

--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,6 @@ export default async function HomePage() {
           <h1 className="font-space-grotesk leading-snug my-8 lg:my-0 mx-6 lg:mx-24 text-center text-[24px] md:text-[40px] font-semibold">
             Et si on apprenait les règles d&apos;orthographe pendant la dictée ?
           </h1>
-          <p className="text-center mx-6 lg:mx-24 text-xl text-gray-600 italic mt-6 font-normal">
-            Fini d&apos;attendre la fin de la dictée pour avoir la correction. Ici, on est en temps réel et les aides pour apprendre l&apos;orthographe sont affichées au fur et à mesure.
-          </p>
         </div>
 
         <div className="flex items-center justify-center h-full mt-0 lg:mt-12">
